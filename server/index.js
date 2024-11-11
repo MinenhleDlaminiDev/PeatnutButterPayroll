30f292eee9e11a8c65745abc3e7e44a4e9267344
MinenhleDlaminiDev <minenhledlaminidev@gmail.com>
2024-11-11 12:14:46 +0200

    Fixed the queries so users can both insert and update without issues

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 // End Point to get all employees with CATCH for errors
 app.get("/employees", async (req, res) => {
     try{
-        const [rows] = await pool.promise().query(`SELECT * FROM Employees`);
+        const [rows] = await pool.promise().query(`SELECT * FROM employees`);
         res.json(rows);
     } catch (err){
         res.status(500).send((err).message);
@@ -21,32 +21,28 @@ app.get("/employees", async (req, res) => {
 
 // End Point to add/update an employee
 app.post("/employee", async (req, res) => {
-    const { EmployeeID, FirstName, LastName, Salutation, Gender, ProfileColor,  GrossSalary } = req.body;
+    const { employeeID, firstName, lastName, salutation, gender, profileColor,  grossSalary, added } = req.body;
     try{
-        const request = new sql.Request();
-
         // if EMPLOYEEID value is true UPDATE if False insert into 
-        const query = EmployeeID
-            ? `UPDATE Employees SET FirstName=@FirstName, LastName=@LastName, Salutation=@Salutation, Gender=@Gender, ProfileColor=@ProfileColor, GrossSalary=@GrossSalary WHERE EmployeeID=EmployeeID`
-            :`INSERT INTO Employees (EmployeeID, FirstName, LastName, Salutation, Gender, ProfileColor, GrossSalary) VALUES (@EmployeeID, @FirstName, @LastName, @Salutation, @Gender, @ProfileColor, @GrossSalary )`;
-        
-        await mysql.query(query, {
-            EmployeeID,
-            FirstName,
-            LastName,
-            Salutation,
-            Gender,
-            ProfileColor,
-            GrossSalary
-        });
-
+        let query;
+        if(added){
+            // Updated existing employees
+            query = `UPDATE employees SET firstName=?, lastName=?, salutation=?, gender=?, profileColor=?, grossSalary=? WHERE employeeID=?`;
+            await pool.promise().query(query, [firstName, lastName, salutation, gender, profileColor, grossSalary, employeeID, added]);
+        } else{
+            // Insert new employees
+            query = `INSERT INTO employees (employeeID, firstName, lastName, salutation, gender, profileColor, grossSalary, added) VALUES (?, ?, ?, ?, ?, ?, ?, 1 )`;
+            await pool.promise().query(query, [employeeID, firstName, lastName, salutation, gender, profileColor, grossSalary, added]);
+        }
+             
         // Sending the success code
         res.sendStatus(200);
     } catch (err) {
-        // error code
+        // Sending Error response
         res.status(500).send((err).message);
     }
 })
 
+// Start server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
